a3ce05267629dc36f94b2f4295b061dd3027929a
Gadiya <kajal.gadiya@capgemini.com>
2020-11-26 02:04:25 +0530

    added unit test

--- src/app/show-details/show-details.component.spec.ts
+++ src/app/show-details/show-details.component.spec.ts
@@ -105,36 +105,36 @@ describe('ShowDetialsComponent', () => {
   }));
 
   it('should set summary value for summary key not found in response', () => {
-    component.showDetailsData = [
-      {
-        "id": 800,
-        "url": "http://www.tvmaze.com/seasons/800/firefly-season-1",
-        "number": 1,
-        "name": "",
-        "episodeOrder": 14,
-        "premiereDate": "2002-09-20",
-        "endDate": "2002-12-20",
-        "network": {
-          "id": 4,
-          "name": "FOX",
-          "country": {
-            "name": "United States",
-            "code": "US",
-            "timezone": "America/New_York"
-          }
-        },
-        "webChannel": null,
-        "image": {
-          "medium": "http://static.tvmaze.com/uploads/images/medium_portrait/9/23969.jpg",
-          "original": "http://static.tvmaze.com/uploads/images/original_untouched/9/23969.jpg"
-        },
-        "_links": {
-          "self": {
-            "href": "http://api.tvmaze.com/seasons/800"
-          }
+    component.showDetailsData =
+    {
+      "id": 800,
+      "url": "http://www.tvmaze.com/seasons/800/firefly-season-1",
+      "number": 1,
+      "name": "",
+      "episodeOrder": 14,
+      "premiereDate": "2002-09-20",
+      "endDate": "2002-12-20",
+      "network": {
+        "id": 4,
+        "name": "FOX",
+        "country": {
+          "name": "United States",
+          "code": "US",
+          "timezone": "America/New_York"
+        }
+      },
+      "webChannel": null,
+      "image": {
+        "medium": "http://static.tvmaze.com/uploads/images/medium_portrait/9/23969.jpg",
+        "original": "http://static.tvmaze.com/uploads/images/original_untouched/9/23969.jpg"
+      },
+      "_links": {
+        "self": {
+          "href": "http://api.tvmaze.com/seasons/800"
         }
       }
-    ]
+    }
+
     component.removeHTMLTagsFromSummary();
     expect(component.showDetailsData.summary.trim()).toEqual('Summary Is unavailable.');
   });
